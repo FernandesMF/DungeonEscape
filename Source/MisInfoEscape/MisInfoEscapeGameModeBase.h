6dfd68653bd9bf1ac5b395c8142e669a52da2c01
FernandesMF <mario.fog.fernandes@gmail.com>
2019-07-29 17:09:36 -0300

    Initial commit, set up gitignore file.
    We won't need to version control lots of files in this project, so gitignore is fundamental.

---
+++ Source/MisInfoEscape/MisInfoEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MisInfoEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MISINFOESCAPE_API AMisInfoEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
