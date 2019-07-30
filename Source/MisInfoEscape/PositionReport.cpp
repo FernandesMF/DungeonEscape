deaaf9de7ee54bcce798dfdc728fa7990bc86128
FernandesMF <mario.fog.fernandes@gmail.com>
2019-07-29 21:44:56 -0300

    Position reporter with actor name.

--- Source/MisInfoEscape/PositionReport.cpp
+++ Source/MisInfoEscape/PositionReport.cpp
@@ -1,6 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "PositionReport.h"
+#include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -17,9 +16,8 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position reporter reporting for %s"), *ObjectName);	
 }
 
 
